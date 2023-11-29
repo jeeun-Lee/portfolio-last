25217355365a412acc3d675b5eb00e2d77e7d228
jeeun-Lee <batch402@gmail.com>
2023-11-29 19:40:59 +0900

    update1129

--- portfolio-type/src/pages/Info.tsx
+++ portfolio-type/src/pages/Info.tsx
@@ -1,16 +1,28 @@
-
+import {styled} from "styled-components";
+const Title = styled.div`
+    width: 100%;
+    font-size: 50px;
+    text-align: center;
+`
+const P_text = styled.p`
+    font-size: 18px;
+`
 function Info() {
     return(
-        <div>
+        <div className="wrap-1400" style={{position: `relative`}}>
+            <Title>
             <h1>
             Thank you <br />
             For Watching!
             </h1>
-
-            <p>
-                Github : <br />
-                e-mail:
-            </p>
+            <P_text>
+                
+                    Github : <br />
+                    e-mail:
+            </P_text>
+            
+            </Title>
+           
         </div>
     )
 }
