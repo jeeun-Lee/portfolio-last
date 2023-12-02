932631f0e1a5f051602e67a029a815ed37c9cae0
jeeun-Lee <batch402@gmail.com>
2023-12-03 01:44:16 +0900

    update1203

--- portfolio-type/src/pages/Skill.tsx
+++ portfolio-type/src/pages/Skill.tsx
@@ -18,53 +18,17 @@ interface skillprops {
 }
 
 function Skill(){
-    const SkillData : skillprops[]=[
-        {
-            id :1,
-            skill: "HTML/CSS",
-            level : 100,  
-            opinion : "웹 표준성/접근성 및 애니메이션 작업에 능숙합니다."
-        },
-        {
-            id :2,
-            skill: "JavaScript",
-            level : 70,  
-            opinion : "바닐라 자바스크립트를 이용한 이벤트 작업을 할 수 있습니다."
-        },
-        {
-            id :3,
-            skill: "jQuery",
-            level : 80,  
-            opinion : "이벤트 및 데이터 출력, 플러그인 사용 등을 작업했습니다."
-        },
-        {
-             id :4,
-            skill: "REACTJS",
-            level : 60,  
-            opinion : "hook을 이용해 데이터 입·출력 할 수 있습니다."
-        },
-        {
-             id :5,
-            skill: "TypeScript",
-            level : 50,  
-            opinion : "타입 지정 및 정의 파일을 작업합니다."
-        },
-        {
-             id :6,
-            skill: "Git",
-            level : 40,  
-            opinion : "Github를 통해 협업 활동을 했습니다."
-        },
-        {
-             id :7,
-            skill: "PhotoShop",
-            level : 40,  
-            opinion : "이미지 편집 등을 했습니다."
-        },
+    const [Skill,setSkill] =useState([]);
+    const SkillData = async() =>{
+        const response = await fetch(`https://raw.githubusercontent.com/jeeun-Lee/portfolio-last/master/portfolio-type/src/assets/skill.json`);
+        const json = await response.json();
+        setSkill(json)
+    }
+    useEffect(()=>{
+        SkillData();
+        
+    },[])
     
-    ]
-
-
     return(
         <section className="Skill">
             <div className="svg-wrap">
@@ -75,7 +39,7 @@ function Skill(){
                     <h1 className="en">Skill Tool</h1>
                     <ul className="skill-list">
                     {
-                        SkillData.map((item:skillprops)=>(
+                        Skill.map((item:skillprops)=>(
                             <li key={item.id}>
                                 <div className="flex align-items-center">
                                     <h6 className="en">{item.skill}</h6>
