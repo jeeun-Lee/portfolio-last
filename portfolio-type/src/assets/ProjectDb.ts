5041c2621fb3e198b3a5e2d1c9f1471ad382738c
jeeun-Lee <batch402@gmail.com>
2023-12-08 21:30:20 +0900

    update1208

--- portfolio-type/src/assets/ProjectDb.ts
+++ portfolio-type/src/assets/ProjectDb.ts
@@ -25,14 +25,22 @@ export const project = [
     },
     {
         id: 4,
+        project_title : "발명특허웹진",
+        type : "웹진",
+        detail : "php 백엔드에서 다양한 레이아웃 작업하고 각 애니메이션들을 구현했습니다.",
+        tag : ["HTML/CSS", "jQuery", "Bootstrap","AOS","Swiper","Git"],
+        url : "http://webzine.kipa.org/web/vol481/"
+    },
+    {
+        id: 5,
         project_title : "HCT 웹진",
         type : "웹진",
-        detail : "php 백엔드에서 각 서브페이지에 레이아웃 구현하고 이전 다음글 기능을 jQuery로 작업했습니다.",
+        detail : "php 백엔드에서 각 서브페이지에 레이아웃 구현하고 이전 다음글 기능 등을 jQuery로 작업했습니다.",
         tag : ["HTML/CSS", "jQuery", "Bootstrap","AOS","Swiper"],
         url : "http://hctinsight.com/webzine/"
     },
     {
-        id: 5,
+        id: 6,
         project_title : "샐러딧 웹사이트",
         type : "웹사이트",
         detail : "php 백엔드에서 CRUD레이아웃 작업하고 각 애니메이션들을 구현했습니다. ",
@@ -40,7 +48,7 @@ export const project = [
         url : "https://saladeat.kr/"
     },
     {
-        id: 6,
+        id: 7,
         project_title : "예술경영지원센터 웹진",
         type : "웹진",
         detail : "ASP.NET 백엔드에서 달마다 발행하는 웹진 업로드 및 유지보수작업을 했습니다.",
@@ -48,7 +56,7 @@ export const project = [
         url : "https://www.gokams.or.kr/webzine/"
     },
     {
-        id: 7,
+        id: 8,
         project_title : "한국벤처캐피탈협회 웹진",
         type : "웹진",
         detail : "php 백엔드에서 달마다 발행하는 웹진 작업에 Chart.js를 활용해 그래프 작업을 했습니다.",
