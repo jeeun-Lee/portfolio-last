5b0d89dcd55ddb07823a37516ab36d031c3dbd65
jeeun-Lee <batch402@gmail.com>
2023-12-29 23:04:54 +0900

    update1229

--- portfolio-type/src/pages/Weather.tsx
+++ portfolio-type/src/pages/Weather.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 interface WeatherResponse {
     weather: {
         id: number;
-
+        main : string;
+        icon : string;
     }[];
 
 }
@@ -17,45 +18,52 @@ function Weather(){
     const [loading, setLoading]  = useState(true);
     const [WeatherD,setWeatherD] = useState<WeatherResponse[]>([]);
     
-    const WeatherData = async(position : GeolocationPosition) =>{
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-        const WEATHER_KEY =process.env.REACT_APP_WEATHER_KEY;
-        const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_KEY}&units=metric`;
-    
-        try {
-            const response = await fetch(weather_url);
-            const json = await response.json();
-            setWeatherD(json);
-            setLoading(false)
-        
-        }catch (error) {
-            console.error("Error fetching weather data:", error);
-        }
-        
-      
-    }
+
+
     const WeatherErr = () =>{
         alert("Not found")
     }
 
-        
-    useEffect(()=>{
-        navigator.geolocation.getCurrentPosition(WeatherData,WeatherErr);
+    
+    const weatherData = async (lat: number, lng: number) =>{
 
+            const WEATHER_KEY =process.env.REACT_APP_WEATHER_KEY;
+            const weather_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${WEATHER_KEY}&units=metric`;
+            console.log(weather_url)
+            const response = await fetch(weather_url);
+            const json = await response.json();
+            setWeatherD([json]);
+            setLoading(false)
+
+    }
+   
+
+    
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lng = position.coords.longitude;
+            weatherData(lat, lng);
+          },WeatherErr);
+
+          
     },[])
     return (
         <div>
-            {loading ? (<div>{loading ? (<h1>Loading</h1>) : WeatherD.length > 0 ? (
-                <div>
-                    {WeatherD.map((item) => (
-                        <p key={item.weather[0].id}>{item.weather[0].id}</p>
-                    ))}
-                </div>
-            ) : (
-                <p>No weather data available</p>
-            )}
-        </div>
+        {loading ? (
+            <h1>Loading</h1>
+        ) : (
+            <div>
+                {Array.isArray(WeatherD) && WeatherD.length > 0 ? (
+                    WeatherD.map((item) => (
+                        <div key={item.weather[0].id}>{item.weather[0].main}, <img src={`http://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt={item.weather[0].main} /></div>
+                    ))
+                ) : (
+                    <p>No weather data available</p>
+                )}
+            </div>
+        )}
+    </div>
     );
 }
 export default Weather;
